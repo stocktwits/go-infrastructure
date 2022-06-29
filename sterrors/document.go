03e430d6456f55434a10ad9489ba9f4f4f4f94ae
Victor Armijo <victor.armijo@bairesdev.com>
2022-06-29 17:29:24 -0600

    add error validation to document

---
+++ sterrors/document.go
@@ -0,0 +1,37 @@
+package sterrors
+
+import (
+	"fmt"
+	"io"
+)
+
+func (e *ErrorFactory) GetDocumentMd(w io.Writer, appname string) error {
+	_, err := fmt.Fprintf(w, "# Application Errors Summary\n\n")
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintf(w, "The following table summarize all the errors that can be expect from %s.\n\n", appname)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintf(w, "|Error Code|Type|Message|HTTP Code|\n")
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintf(w, "|:----------|:----------|:----------|:----------|\n")
+	if err != nil {
+		return err
+	}
+
+	for code, info := range e.config {
+		_, err = fmt.Fprintf(w, "|%d|%s|%s|%d|\n", code, info.Type, info.Message, info.Http_code)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
